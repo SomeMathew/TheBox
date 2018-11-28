9b009042d7cbcdb7cf8083aea1796ac856f2ab41
SomeMatthew <someMathew@outlook.com>
2018-11-28 09:50:15 -0500

    Error in ioctl.h, fixed

--- avr/libs/inc/ioctl.h
+++ avr/libs/inc/ioctl.h
@@ -8,35 +8,35 @@
 #define OUTPUT (1)
 #define INPUT (0)
 
-static inline void ioctl_setdir(uint8_t ddrReg, uint8_t io, uint8_t dir) {
+static inline void ioctl_setdir(volatile uint8_t * ddrReg, uint8_t io, uint8_t dir) {
 	switch (dir) {
 		case INPUT:
-			ddrReg &= ~(_BV(io));
+			*ddrReg &= ~(_BV(io));
 			break;
 		case OUTPUT:
-			ddrReg |= _BV(io);
+			*ddrReg |= _BV(io);
 			break;
 	}
 }
 
-static inline void ioctl_pullup(uint8_t portReg, uint8_t io) {
-	portReg |= _BV(io);
+static inline void ioctl_pullup(volatile uint8_t * portReg, uint8_t io) {
+	*portReg |= _BV(io);
 }
 
-static inline bool ioctl_read(uint8_t pinReg, uint8_t io) {
-	return pinReg & _BV(io);
+static inline bool ioctl_read(volatile uint8_t * pinReg, uint8_t io) {
+	return *pinReg & _BV(io);
 }
 
-static inline void ioctl_write(uint8_t portReg, uint8_t io, bool val) {
+static inline void ioctl_write(volatile uint8_t * portReg, uint8_t io, bool val) {
 	if (val) {
-		portReg |= _BV(io);
+		*portReg |= _BV(io);
 	} else {
-		portReg &= ~(_BV(io));
+		*portReg &= ~(_BV(io));
 	}
 }
 
-static inline void ioctl_toggle(uint8_t portReg, uint8_t io) {
-	portReg ^= _BV(io);
+static inline void ioctl_toggle(volatile uint8_t * portReg, uint8_t io) {
+	*portReg ^= _BV(io);
 } 
 
 #endif /* _DEV_IOCTL_H */

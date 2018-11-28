89a267a79d46b79e053c0d0d39db8d800c423d78
SomeMatthew <someMathew@outlook.com>
2018-11-28 08:43:36 -0500

    Pin config header

---
+++ avr/inc/pin_config.h
@@ -0,0 +1,29 @@
+#ifndef _DEV_IO_CONFIG_H
+#define _DEV_IO_CONFIG_H
+
+#define SERVO_A_DDR 	DDRB
+#define SERVO_A_IO 		PB1
+
+#define SERVO_B_DDR 	DDRC
+#define SERVO_B_PORT	PORTC
+#define SERVO_B_PIN		PINC
+#define SERVO_B_IO 		PC0
+
+#define LED_ALIVE_DDR	DDRD
+#define LED_ALIVE_PORT	PORTD
+#define LED_ALIVE_PIN	PIND
+#define LED_ALIVE_IO	PD4
+
+#define BBB_STATUS_DDR	DDRD
+#define BBB_STATUS_PORT	PORTD
+#define BBB_STATUS_PIN	PIND
+#define BBB_STATUS_IO	PD5
+
+#define BOX_SWITCH_DDR	DDRC
+#define BOX_SWITCH_PORT	PORTC
+#define BOX_SWITCH_PIN	PINC
+#define BOX_SWITCH_IO	PC3
+
+#define 
+
+#endif /* _DEV_IO_CONFIG_H */
---
+++ avr/libs/inc/ioctl.h
@@ -0,0 +1,42 @@
+#ifndef _DEV_IOCTL_H
+#define _DEV_IOCTL_H
+
+#include <stdbool.h>
+#include <avr/io.h>
+#include <avr/sfr_defs.h>
+
+#define OUTPUT (1)
+#define INPUT (0)
+
+static inline void ioctl_setdir(uint8_t ddrReg, uint8_t io, uint8_t dir) {
+	switch (dir) {
+		case INPUT:
+			ddrReg &= ~(_BV(io));
+			break;
+		case OUTPUT:
+			ddrReg |= _BV(io);
+			break;
+	}
+}
+
+static inline void ioctl_pullup(uint8_t portReg, uint8_t io) {
+	portReg |= _BV(io);
+}
+
+static inline bool ioctl_read(uint8_t pinReg, uint8_t io) {
+	return pinReg & _BV(io);
+}
+
+static inline void ioctl_write(uint8_t portReg, uint8_t io, bool val) {
+	if (val) {
+		portReg |= _BV(io);
+	} else {
+		portReg &= ~(_BV(io));
+	}
+}
+
+static inline void ioctl_toggle(uint8_t portReg, uint8_t io) {
+	portReg ^= _BV(io);
+} 
+
+#endif /* _DEV_IOCTL_H */

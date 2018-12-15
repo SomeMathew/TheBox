ad4bbb50581347828eb5e598f8380b9b6342e617
Richard <mcccaughey@gmail.com>
2018-12-15 13:48:20 -0500

    Split the facial recognition into 3 separate functions, load picture, load unknown image, and the test to see a match

--- bbb/person_recognition.py
+++ bbb/person_recognition.py
@@ -1,32 +1,34 @@
 import face_recognition
 import os
 
-def person_recognition(unknown_face):
+def load_images(known_images):
     known_dir = "/var/lib/cloud9/theBox/known/" # Directory holding all of the known images
     directory = os.fsencode(known_dir)
 
     # These lists will hold the image files and their encodings
-    known_images = []
-    known_encodings = []
 
     # Load all the known images
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         known_images.append(face_recognition.load_image_file(known_dir + filename))
 
+def load_unknown_image(known_images, known_encodings, unknown_face):
     # Load the unknown image
     unknown_image = face_recognition.load_image_file(unknown_face)
-
     # Try to encode all the images
     try:
-        for image in known_images:
-            known_encodings.append(face_recognition.face_encodings(image)[0]) #0th face
+        #If known encodings is empty, encode all known images
+        if not known_encodings:
+            for image in known_images:
+                known_encodings.append(face_recognition.face_encodings(image)[0]) #0th face
+        #Encodings already loaded
         unknown = face_recognition.face_encodings(unknown_image)[0]
     except IndexError:
             print("Could not find faces...")
-            return False
             quit()
+    return unknown
 
+def person_recognition(unknown, known_encodings):
     results = face_recognition.compare_faces(known_encodings, unknown)
 
     # Check if there is a valid match
